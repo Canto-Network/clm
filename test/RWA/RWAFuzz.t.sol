5c9473b06955e7c5db7c6c4433136bf1407cae11
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-07-19 17:25:04 -0400

    fuzz testing

--- test/RWA/RWAFuzz.t.sol
+++ test/RWA/RWAFuzz.t.sol
@@ -6,14 +6,42 @@ import "./RWASetup.sol";
 import {RWACErc20} from "src/RWA/CErc20_RWA.sol";
 
 contract RWAFuzz is StdInvariant, RWASetup {
+    address targetAccount = address(1234567);
+    address[] accounts = [address(123), address(321)];
+
+    uint TARGET_ACCOUNT_INITIAL_BALANCE = 1000 ether;
 
     function setUp() public override {
-        targetSender(admin);
+        // set up target msg.senders
+        targetSender(targetAccount);
+        for (uint i; i < accounts.length; i++) {
+            targetSender(accounts[i]);
+        }
+        // call normal setup
         super.setUp();
+
+        // add rwaCToken market
+        prankAddCTokenMarket(address(rwaCToken), 0.9e18);
+
+        // send underlying to targetAccount
+        vm.prank(admin);
+        rwaUnderlying.transfer(targetAccount, TARGET_ACCOUNT_INITIAL_BALANCE);
     }
-    function invariant_1() external {
-        // assert(isCTokenMarket(address(rwaCToken)));
-        // supplyToken(admin, address(rwaUnderlying), address(rwaCToken), 1e18);
-        assert(rwaCToken.balanceOf(admin) == 0);
+
+    function invariant_accountBalances() external {
+        for (uint i; i < accounts.length; i++) {
+            assertEq(rwaCToken.balanceOf(accounts[i]), 0);
+            assertEq(rwaUnderlying.balanceOf(accounts[i]), 0);
+        }
+    }
+    function test_targetAccountSupply(uint _supplyAmount) external {
+        vm.assume(_supplyAmount > 0 && _supplyAmount <= TARGET_ACCOUNT_INITIAL_BALANCE);
+        supplyToken(
+            targetAccount,
+            address(rwaUnderlying),
+            address(rwaCToken),
+            _supplyAmount
+        );
+        require(rwaCToken.balanceOf(targetAccount) == _supplyAmount, "targetAccount balance should be equal to supply amount");
     }
 }

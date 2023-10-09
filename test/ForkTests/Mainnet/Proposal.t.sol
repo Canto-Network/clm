af48617320f2e727d0c27f37f37e76c94fc34146
T.K. Kwon <tkkwon1998@gmail.com>
2023-10-09 16:17:41 +0900

    fix failing test

--- test/ForkTests/Mainnet/Proposal.t.sol
+++ test/ForkTests/Mainnet/Proposal.t.sol
@@ -7,6 +7,76 @@ import "../../../src/Comptroller.sol";
 import "../../../src/Unitroller.sol";
 import "../../../src/Governance/GovernorBravoDelegate.sol";
 
+contract Upgrade is Test {
+    uint256 mainnetFork;
+
+    address unitroller = 0x5E23dC409Fc2F832f83CEc191E245A191a4bCc5C;
+    address comptrollerAddress = 0x0fbc04D1ac348BBD2126246f57EeA08290a56A79; // new comptrollerV2 address
+
+    address proposalStore = 0x648a5Aa0C4FbF2C1CF5a3B432c2766EeaF8E402d;
+    address govshuttle = 0xB7a78c42532424AC7B2584F02E58C6699c660925;
+    address timelock = 0xF33942F457EcabF2a03828e299C052A9523cc473;
+    address governorBravoDelegator = 0xBC3139f9dA6b16A8FF8Ac6e0dEc4C0278d532dba;
+
+    Unitroller unitrollerContract = Unitroller(payable(unitroller));
+    ProposalStore store = ProposalStore(proposalStore);
+    GovernorBravoDelegate gov = GovernorBravoDelegate(governorBravoDelegator);
+
+    address[] targets;
+    string[] signatures;
+    bytes[] calldatas;
+    uint256[] values;
+
+    function setUp() public {
+        mainnetFork = vm.createFork("https://mainnode.plexnode.org:8545");
+        vm.selectFork(mainnetFork);
+    }
+
+    function upgrade() public {
+        // tx to set pending implementation
+        bytes32 leftPaddedAddress = bytes32(uint256(uint160(comptrollerAddress)));
+        bytes memory i_bytes = abi.encodePacked(leftPaddedAddress);
+        targets.push(unitroller);
+        values.push(0);
+        signatures.push("_setPendingImplementation(address)");
+        calldatas.push(i_bytes);
+
+        // tx to accept implementation
+        bytes32 leftPaddedUnitroller = bytes32(uint256(uint160(unitroller)));
+        i_bytes = abi.encodePacked(leftPaddedUnitroller);
+        targets.push(comptrollerAddress);
+        values.push(0);
+        signatures.push("_become(address)");
+        calldatas.push(i_bytes);
+
+        vm.startPrank(govshuttle); // prank as govshuttle so we can append to proposal store
+
+        // add proposal to store
+        store.AddProposal(
+            129, "upgrade comptroller", "proposal to upgrade comptroller", targets, values, signatures, calldatas
+        );
+
+        vm.stopPrank();
+
+        // queue proposal
+        gov.queue(129);
+        skip(3600);
+
+        // execute proposal
+        gov.execute(129);
+    }
+
+    function test_addProposal() public {
+        // check implementation before upgrade
+        assertEq(unitrollerContract.comptrollerImplementation(), 0xD5DbF5cd90f158597f916591dBaDDe27E4A4d4Cf);
+
+        upgrade();
+
+        // check implementation after upgrade
+        assertEq(unitrollerContract.comptrollerImplementation(), comptrollerAddress);
+    }
+}
+
 interface ProposalStore {
     struct Proposal {
         // @notice Unique id for looking up a proposal
@@ -33,74 +103,4 @@ interface ProposalStore {
     ) external;
 
     function QueryProp(uint256 propId) external returns (Proposal memory);
-}
-
-contract Upgrade is Test {
-    uint256 mainnetFork;
-
-    address unitroller = 0x5E23dC409Fc2F832f83CEc191E245A191a4bCc5C;
-
-    address proposalStore = 0x648a5Aa0C4FbF2C1CF5a3B432c2766EeaF8E402d;
-    address govshuttle = 0xB7a78c42532424AC7B2584F02E58C6699c660925;
-    address timelock = 0xF33942F457EcabF2a03828e299C052A9523cc473;
-    address governorBravoDelegator = 0xBC3139f9dA6b16A8FF8Ac6e0dEc4C0278d532dba;
-
-    Unitroller unitrollerContract = Unitroller(payable(unitroller));
-    ProposalStore store = ProposalStore(proposalStore);
-    GovernorBravoDelegate gov = GovernorBravoDelegate(governorBravoDelegator);
-
-    address[] targets;
-    string[] signatures;
-    bytes[] calldatas;
-    uint256[] values;
-
-    function setUp() public {
-        mainnetFork = vm.createFork("https://mainnode.plexnode.org:8545");
-        vm.selectFork(mainnetFork);
-    }
-
-    function upgrade() public {
-        vm.startPrank(govshuttle);
-
-        // tx to set pending implementation
-        address comptrollerAddress = 0x0fbc04D1ac348BBD2126246f57EeA08290a56A79;
-        bytes32 leftPaddedAddress = bytes32(uint256(uint160(comptrollerAddress)));
-        bytes memory i_bytes = abi.encodePacked(leftPaddedAddress);
-        targets.push(unitroller);
-        values.push(0);
-        signatures.push("_setPendingImplementation(address)");
-        calldatas.push(i_bytes);
-
-        // tx to accept implementation
-        bytes32 leftPaddedUnitroller = bytes32(uint256(uint160(unitroller)));
-        i_bytes = abi.encodePacked(leftPaddedUnitroller);
-        targets.push(comptrollerAddress);
-        values.push(0);
-        signatures.push("_become(address)");
-        calldatas.push(i_bytes);
-
-        // add proposal to store
-        store.AddProposal(
-            129, "upgrade comptroller", "proposal to upgrade comptroller", targets, values, signatures, calldatas
-        );
-
-        vm.stopPrank();
-
-        // queue proposal
-        gov.queue(129);
-        skip(3600);
-
-        // execute proposal
-        gov.execute(129);
-    }
-
-    function test_addProposal() public {
-        // check implementation before upgrade
-        assertEq(unitrollerContract.comptrollerImplementation(), 0xD5DbF5cd90f158597f916591dBaDDe27E4A4d4Cf);
-
-        upgrade();
-
-        // check implementation after upgrade
-        assertEq(unitrollerContract.comptrollerImplementation(), comptrollerAddress);
-    }
 }
